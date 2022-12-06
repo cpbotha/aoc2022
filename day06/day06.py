e69a05bd5c2eefd8b908fee566838749757cdbf3
Charl P. Botha <cpbotha@vxlabs.com>
2022-12-06 08:48:15 +0200

    Fix comments, remove unused var

--- day06/day06.py
+++ day06/day06.py
@@ -1,23 +1,22 @@
 # start at 7:49, p1 at 7:54
+# p2 at 7:57ish
 
-# %%
+# %% read data
 from collections import Counter
 from pathlib import Path
 
-fn = Path(__file__).parent / "test_input_p2_1.txt"
+fn = Path(__file__).parent / "input.txt"
 
-# note the rstrip() -- we don't want to remove whitespace from the first line!
 datastream = fn.read_text().strip()
 
-# %% part 1
+# %% part 1 = 1920
 for i in range(len(datastream) - 4):
     c = Counter(datastream[i:i+4])
     if all((v == 1 for v in c.values())):
         print(i+4)
         break
 
-# %% part 2
-num = 14
+# %% part 2 = 2334
 for i in range(len(datastream) - 14):
     c = Counter(datastream[i:i+14])
     if all((v == 1 for v in c.values())):

a6a06f218d9459bf206116a59a2db51fc7ed9bc2
Charl P. Botha <cpbotha@vxlabs.com>
2022-12-10 11:48:11 +0200

    add part 2 solution

--- day10/day10.py
+++ day10/day10.py
@@ -1,3 +1,5 @@
+# this took me about an hour in total, with interruptions and Saturday morning stuff
+
 # %% read data
 from pathlib import Path
 
@@ -21,14 +23,36 @@ def maybe_measure_cycle(check_cycle):
         sum_strength += check_cycle * reg_x
 
 
+# part 2: reg_x is 0-based position of centre pixel of XXX sprite
+def draw_pixel():
+    # cycle is pixel currently being drawn:
+    # while cycle is 1-based, ALL positions are 0 based!
+    pixel_x = (cycle - 1) % 40
+    sprite_cx = reg_x % 40
+    sprite = [sprite_cx-1, sprite_cx, sprite_cx+1]
+    if sprite[0] < 0:
+        del sprite[0]
+    if sprite[-1] > 39:
+        del sprite[-1]
+    ret = "#" if pixel_x in sprite else "."
+    if pixel_x == 39:
+        # pixels at the right end, add a \n
+        ret += "\n"
+    return ret
+
+
+disp = ""
 for ip in ins:
+    disp += draw_pixel()
     match ip.split(" "):
         case ["noop"]:
             cycle += 1
             maybe_measure_cycle(cycle)
+
         case ["addx", v]:
             cycle += 1
             maybe_measure_cycle(cycle)
+            disp += draw_pixel()
             cycle += 1
             reg_x += int(v)
             maybe_measure_cycle(cycle)
@@ -38,3 +62,18 @@ for ip in ins:
 
 print(f"part1 sum of signal strengths: {sum_strength}")
 
+# PLULKBZH
+print(f"\n{disp}")
+
+"""
+# wow, I had to squint really hard to read, and then harder to guess the P
+# my test pattern was 100%
+
+###..#....#..#.#....#..#.###..####.#..##
+...#.#....#..#.#....#.#..#..#....#.#..#.
+#..#.#....#..#.#....##...###....#..####.
+###..#....#..#.#....#.#..#..#..#...#..#.
+.....#....#..#.#....#.#..#..#.#....#..##
+.....####..##..####.#..#.###..####.#..#.
+
+"""
